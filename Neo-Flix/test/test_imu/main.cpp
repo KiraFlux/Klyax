1cedcffbc25a4c86123571e590b269e6beb264b8
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-08-21 16:42:43 +0300

    test_imu LFU gyro ok

--- Neo-Flix/test/test_imu/main.cpp
+++ Neo-Flix/test/test_imu/main.cpp
@@ -23,10 +23,12 @@ void setup() {
     while (not imu.init(GPIO_NUM_18, GPIO_NUM_19, GPIO_NUM_23, GPIO_NUM_5)) {
         delay(1000);
     }
+
+    imu.calibrateGyro(5000);
 }
 
 ela::vec3f fromIMUtoFLU(float x, float y, float z) {
-    return ela::vec3f{-y, +x, -z} * 0.001f;
+    return ela::vec3f{-y, +x, -z};
 }
 
 void loop() {
@@ -34,18 +36,35 @@ void loop() {
 
     imu.imu.getAGMT();
 
-    ela::vec3f acc = fromIMUtoFLU(imu.imu.accX(), imu.imu.accY(), imu.imu.accZ());
+//    ela::vec3f acc = fromIMUtoFLU(imu.imu.accX(), imu.imu.accY(), imu.imu.accZ());
 
-    float acc_roll = std::atan2(acc.y, -acc.z);
-    float acc_pitch = std::atan2(-acc.x, std::hypot(acc.y, acc.z));
+    ela::vec3f gyro = fromIMUtoFLU(-imu.imu.gyrX(), -imu.imu.gyrY(), -imu.imu.gyrZ());
 
-    Serial.printf(
-        "A[%+.2f %+.2f %+.2f]\t"
-        "Roll: %+3.1f\tPitch: %+3.1f\n",
-        acc.x, acc.y, acc.z,
-        acc_roll * RAD_TO_DEG,
-        acc_pitch * RAD_TO_DEG
-    );
+    static ela::vec3f orient{};
+    constexpr float dt = 1e-3;
+//    float acc_roll = std::atan2(acc.y, -acc.z);
+//    float acc_pitch = std::atan2(-acc.x, std::hypot(acc.y, acc.z));
 
-    delay(50);
+    orient += gyro * dt;
+
+    static int i = 0;
+    i += 1;
+    if (i == 100) {
+        i = 0;
+        Serial.printf(
+            "G[%+f %+f %+f]\n",
+            orient.x, orient.y, orient.z
+        );
+    }
+
+
+//    Serial.printf(
+//        "A[%+.2f %+.2f %+.2f]\t"
+//        "Roll: %+3.1f\tPitch: %+3.1f\n",
+//        acc.x, acc.y, acc.z,
+//        acc_roll * RAD_TO_DEG,
+//        acc_pitch * RAD_TO_DEG
+//    );
+
+    delay(1);
 }
